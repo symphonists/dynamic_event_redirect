40f5b3e8821111c234d80a0559669a7f2087a3a4
max <max@icelab.com.au>
2009-04-01 16:19:57 +1100

    Initial commit

---
+++ extension.driver.php
@@ -0,0 +1,121 @@
+<?php
+	Class extension_dynamic_event_redirect extends Extension
+	{
+		/*-------------------------------------------------------------------------
+			Extension definition
+		-------------------------------------------------------------------------*/
+		public function about()
+		{
+			return array(
+				'name' => 'Dynamic Event Redirect',
+				'version'	=> '0.0.1',
+				'author'	=> array('name' => 'Max Wheeler',
+									'website' => 'http://makenosound.com/',
+									'email' => 'max@makenosound.com'),
+				'release-date' => '2009-04-01',
+			);
+		}
+		
+		/*-------------------------------------------------------------------------
+			Delegate
+		-------------------------------------------------------------------------*/
+		public function getSubscribedDelegates()
+		{
+			return array(
+				array(
+					'page'		=> '/blueprints/events/edit/',
+					'delegate'	=> 'AppendEventFilter',
+					'callback'	=> 'add_filter_to_event_editor'
+				),				
+				array(
+					'page'		=> '/blueprints/events/new/',
+					'delegate'	=> 'AppendEventFilter',
+					'callback'	=> 'add_filter_to_event_editor'
+				),
+				array(
+				'page' => '/blueprints/events/new/',
+					'delegate' => 'AppendEventFilterDocumentation',
+					'callback' => 'add_filter_documentation_to_event'
+				),					
+				array(
+				'page' => '/blueprints/events/edit/',
+					'delegate' => 'AppendEventFilterDocumentation',
+					'callback' => 'add_filter_documentation_to_event'
+				),
+				array(
+					'page'		=> '/frontend/',
+					'delegate'	=> 'EventFinalSaveFilter',
+					'callback'	=> 'process_redirect'
+				),
+			);
+		}
+		
+		/*-------------------------------------------------------------------------
+			Custom functions
+		-------------------------------------------------------------------------*/	
+		
+		public function add_filter_to_event_editor(&$context)
+		{
+			$context['options'][] = array('dynamic-event-redirect', @in_array('dynamic-event-redirect', $context['selected']) ,'Dynamic event redirection');
+		}
+		
+		public function add_filter_documentation_to_event($context)
+		{
+			if ( ! in_array('dynamic-event-redirect', $context['selected'])) return;
+			
+			$context['documentation'][] = new XMLElement('h3', 'Dynamic Event Redirect');
+			$context['documentation'][] = new XMLElement('p', 'To use, simply add a hidden input field to your form that has the name <code>der-params</code> and its value as a comma separated list of parameters you wish to include. For example, so to pass the variable <code>email</code> on you&#8217;d do something like:');
+			$code = '<input name="fields[email]" type="text" />
+<input type="hidden" name="der-params" value="email,foo:bar"/>';
+			$context['documentation'][] = contentBlueprintsEvents::processDocumentationCode($code);
+			$context['documentation'][] = new XMLElement('p', 'You can also output values directly by using key:value pairs.');
+			$context['documentation'][] = new XMLElement('h3', 'Clean URL Params');
+			$context['documentation'][] = new XMLElement('p', 'If you&#8217;re using Rowan&#8217;s <a href="http://overture21.com/forum/comments.php?DiscussionID=795">Clean URL Params</a> extension you can set the output to use clean syntax by adding the following to your form:');
+			$code = '<input name="der-format" type="1" />';
+			$context['documentation'][] = contentBlueprintsEvents::processDocumentationCode($code);
+		}
+				
+		public function process_redirect($context)
+		{
+			# Check if in included filters
+			if ( ! in_array('dynamic-event-redirect', $context['event']->eParamFILTERS)) return;
+			if ( in_array('expect-multiple', $context['event']->eParamFILTERS)) return;
+			
+			$mapping = explode(",", $_POST['der-params']);
+			$redirect = $_POST['redirect'];
+			$cleanparams = (isset($_POST['der-format']) && $_POST['der-format'] == 1) ? TRUE : FALSE;
+			
+			$data = array();
+			if(isset($mapping) && $redirect)
+			{
+				foreach ($mapping as $key)
+				{
+					# If there's a match, map the value of the match. Else output the value.
+					if(isset($context['fields'][$key]))
+					{
+						$data[$key] = $context['fields'][$key];
+					}
+					else if($key == 'id')
+					{
+						$data['id'] = $context['entry']->get('id');
+					}
+					else
+					{
+						$pair = explode(":", $key);
+						$data[$pair[0]] = $pair[1];
+					}
+				}
+				$encoded_data = "";
+				# Setup cleanurlparams ... or not
+				$join = ($cleanparams) ? ":" : "=";
+				$separator = ($cleanparams) ? "/" : "&";
+				$start = ($cleanparams) ? "/" : "?";
+				foreach ($data as $key => $val)
+				{
+					$encoded_data[] = urlencode($key) . $join . urlencode($val);
+				}
+				redirect($redirect . $start . implode($separator, $encoded_data));
+			}
+			return;
+		}
+	}
